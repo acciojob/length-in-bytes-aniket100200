66145f09c79a9d0fad1e10d2064acceeaa434e30
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-12-20 08:57:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const byteSize = (str) => {
   // write your code here
+	let b=new Blob([str]);
+	
+	return b.size;
 };
 
 // Do not change the code below
 const str = prompt("Enter some string.");
+
 alert(byteSize(str));
